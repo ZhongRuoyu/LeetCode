f123344131a5cd35625517d17822080eadc44d61
Ruoyu <zhongruoyu@outlook.com>
2021-01-26 09:14:47 +0800

    Problem added

---
+++ problems/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,10 @@
+// Solved 2021-01-26 09:14
+// Runtime: 0 ms (100.00%)
+// Memory Usage: 5.7 MB (99.02%)
+
+class Solution {
+   public:
+    int numberOfMatches(int n) {
+        return (n - 1);
+    }
+};
