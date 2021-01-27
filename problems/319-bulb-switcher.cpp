a3439be94f2a141d0f8e9ba99a35b7cd840183cc
Ruoyu <zhongruoyu@outlook.com>
2021-01-27 09:08:53 +0800

    Problem added

---
+++ problems/319-bulb-switcher.cpp
@@ -0,0 +1,10 @@
+// Solved 2021-01-27 09:08
+// Runtime: 0 ms (100.00%)
+// Memory Usage: 5.9 MB (91.58%)
+
+class Solution {
+   public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
