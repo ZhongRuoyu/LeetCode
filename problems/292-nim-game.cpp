209fb12bad3451c5b6d731419e6032d20ad0674e
Ruoyu <zhongruoyu@outlook.com>
2021-01-29 10:20:06 +0800

    Problem added

---
+++ problems/292-nim-game.cpp
@@ -0,0 +1,10 @@
+// Solved 2021-01-29 10:19
+// Runtime: 0 ms (100.00%)
+// Memory Usage: 5.8 MB (93.16%)
+
+class Solution {
+   public:
+    bool canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+};
