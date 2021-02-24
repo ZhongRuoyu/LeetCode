1a5df41f093b2498f380dcfd4888f03177db9692
ZhongRuoyu <zhongruoyu@outlook.com>
2021-02-24 09:30:44 +0800

    Problem added

---
+++ problems/617-merge-two-binary-trees.cpp
@@ -0,0 +1,26 @@
+// Solved 2021-02-24 09:29
+// Runtime: 28 ms (98.30%)
+// Memory Usage: 32.3 MB (96.53%)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    TreeNode *mergeTrees(TreeNode *t1, TreeNode *t2) {
+        if (t1 == nullptr) return t2;
+        if (t2 == nullptr) return t1;
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};
