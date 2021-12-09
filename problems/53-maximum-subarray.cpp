08cbfe6580138a08c9f7e7d565ee725e0cb9c9b9
Zhong Ruoyu <zhongruoyu@outlook.com>
2021-12-09 12:36:09 +0800

    Update problem 53

--- problems/53-maximum-subarray.cpp
+++ problems/53-maximum-subarray.cpp
@@ -15,3 +15,43 @@ class Solution {
         return max;
     }
 };
+
+/*
+
+A divide-and-conquer approach:
+
+class Solution {
+    struct Sums {
+        int all_sum;
+        int left_sum;
+        int right_sum;
+        int max_sum;
+    };
+
+   public:
+    int maxSubArray(vector<int> &nums) {
+        return maxSubArray(nums, 0, nums.size()).max_sum;
+    }
+
+   private:
+    Sums Combine(Sums lhs, Sums rhs) {
+        int all_sum = lhs.all_sum + rhs.all_sum;
+        int left_sum = max(lhs.left_sum, lhs.all_sum + rhs.left_sum);
+        int right_sum = max(rhs.right_sum, lhs.right_sum + rhs.all_sum);
+        int max_sum =
+            max(max(lhs.max_sum, rhs.max_sum), lhs.right_sum + rhs.left_sum);
+        return (Sums){all_sum, left_sum, right_sum, max_sum};
+    }
+
+    Sums maxSubArray(vector<int> &nums, size_t start, size_t end) {
+        if (start + 1 == end) {
+            return (Sums){nums[start], nums[start], nums[start], nums[start]};
+        }
+        size_t mid = start + (end - start) / 2;
+        Sums lhs = maxSubArray(nums, start, mid);
+        Sums rhs = maxSubArray(nums, mid, end);
+        return Combine(lhs, rhs);
+    }
+};
+
+*/
