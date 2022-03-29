157a5d3c136bebbac0761f44f3e5da8f78ed0b34
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-03-29 13:18:09 +0800

    Add problem 1179

---
+++ problems/1179-reformat-department-table.sql
@@ -0,0 +1,20 @@
+-- Solved 2022-03-29
+-- Runtime: 431 ms (92.37%)
+-- Memory Usage: 0 B (100.00%)
+
+# Write your MySQL query statement below
+SELECT id,
+    SUM(CASE month WHEN 'Jan' THEN revenue ELSE NULL END) AS Jan_Revenue,
+    SUM(CASE month WHEN 'Feb' THEN revenue ELSE NULL END) AS Feb_Revenue,
+    SUM(CASE month WHEN 'Mar' THEN revenue ELSE NULL END) AS Mar_Revenue,
+    SUM(CASE month WHEN 'Apr' THEN revenue ELSE NULL END) AS Apr_Revenue,
+    SUM(CASE month WHEN 'May' THEN revenue ELSE NULL END) AS May_Revenue,
+    SUM(CASE month WHEN 'Jun' THEN revenue ELSE NULL END) AS Jun_Revenue,
+    SUM(CASE month WHEN 'Jul' THEN revenue ELSE NULL END) AS Jul_Revenue,
+    SUM(CASE month WHEN 'Aug' THEN revenue ELSE NULL END) AS Aug_Revenue,
+    SUM(CASE month WHEN 'Sep' THEN revenue ELSE NULL END) AS Sep_Revenue,
+    SUM(CASE month WHEN 'Oct' THEN revenue ELSE NULL END) AS Oct_Revenue,
+    SUM(CASE month WHEN 'Nov' THEN revenue ELSE NULL END) AS Nov_Revenue,
+    SUM(CASE month WHEN 'Dec' THEN revenue ELSE NULL END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
