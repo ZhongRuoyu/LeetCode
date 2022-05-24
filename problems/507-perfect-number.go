9fa749c992bd81e0e52172331dd3c8877f262f23
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-05-24 08:08:09 +0800

    Add problem 507

---
+++ problems/507-perfect-number.go
@@ -0,0 +1,7 @@
+// Solved 2022-05-24
+// Runtime: 0 ms (100.00%)
+// Memory Usage: 1.8 MB (86.96%)
+
+func checkPerfectNumber(num int) bool {
+	return num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336
+}
