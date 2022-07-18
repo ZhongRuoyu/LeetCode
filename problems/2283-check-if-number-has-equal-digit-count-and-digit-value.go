cba12f80f877b39927084b31dfbd02cc2d36a1a5
Zhong Ruoyu <zhongruoyu@outlook.com>
2022-07-18 11:41:29 +0800

    Add problem 2283

---
+++ problems/2283-check-if-number-has-equal-digit-count-and-digit-value.go
@@ -0,0 +1,13 @@
+// Solved 2022-07-18
+// Runtime: 0 ms (100.00%)
+// Memory Usage: 2 MB (82.35%)
+
+func digitCount(num string) bool {
+	return num == "1210" ||
+		num == "2020" ||
+		num == "21200" ||
+		num == "3211000" ||
+		num == "42101000" ||
+		num == "521001000" ||
+		num == "6210001000"
+}
